ddbdf7db9cc05e003234199b19cfd80c4459e957
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-05-15 20:56:32 +0530

    Another part added to FeatureSection1.jsx

--- src/components/featureSection1.jsx
+++ src/components/featureSection1.jsx
@@ -75,7 +75,7 @@ export function FeatureSectionOne() {
 
                         </div>
 
-                        <div className="flex items-center gap-5">
+                        <div className="flex items-center gap-5 flex-col md:flex-row">
 
                             <a href="#" rel="noopener noreferrer" >
                                 <button className="!text-white !font-bold cursor-pointer !bg-blue-500 !rounded-md h-[50px] w-[160px] flex items-center justify-center gap-1.5">
@@ -87,7 +87,7 @@ export function FeatureSectionOne() {
                                 </button>
                             </a>
 
-                            <a href="#" className="text-[16px] font-bold !text-[#2B84EA] hover:text-blue-100 ">
+                            <a href="#" className="text-[16px] font-bold !text-[#0B72E7] hover:text-blue-100 ">
                                 Know More {'>'}
                             </a>
                         </div>
@@ -100,6 +100,147 @@ export function FeatureSectionOne() {
 
                 </div>
 
+            </div>
+
+
+            {/* Another Part */}
+            <div className="w-full flex items-center justify-center !mt-10">
+
+                <div className=" w-[71%] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-3">
+
+                    {/* Box1 */}
+                    <div className="cursor-pointer !p-5 flex flex-col gap-4">
+
+                        <div className="flex items-center justify-between">
+                            <h2 className="text-black !text-[22px] !font-bold">
+                                Payment Links
+                            </h2>
+
+                            <img src="./src/assets/payment-link-icon.svg" alt="" className="bg-[#2B84EA] rounded-4xl w-12 h-12 object-contain" />
+                        </div>
+
+                        <div className="flex flex-col gap-11">
+                            <p className="!text-[16px] text-[#818597] !leading-6">
+                                Create, Copy & Collect With Payment Button. Collect one time or subscription payments & more
+                            </p>
+
+                            <a href="#" className="text-[16px] font-bold !text-[#0B72E7] hover:text-blue-100 ">
+                                Know More {'>'}
+                            </a>
+                        </div>
+                    </div>
+
+                    {/* Box2 */}
+                    <div className="cursor-pointer !p-5 flex flex-col gap-4">
+                        
+                        <div className="flex items-center justify-between">
+                            <h2 className="text-black !text-[22px] !font-bold">
+                                Payment Links
+                            </h2>
+
+                            <img src="./src/assets/payment-link-icon.svg" alt="" className="bg-[#2B84EA] rounded-4xl size-12" />
+                        </div>
+
+                        <div className="flex flex-col gap-11">
+                            <p className="!text-[16px] text-[#818597] !leading-6">
+                                Create, Copy & Collect With Payment Button. Collect one time or subscription payments & more
+                            </p>
+
+                            <a href="#" className="text-[16px] font-bold !text-[#0B72E7] hover:text-blue-100 ">
+                                Know More {'>'}
+                            </a>
+                        </div>
+                    </div>
+
+                    {/* Box3 */}
+                    <div className="cursor-pointer !p-5 flex flex-col gap-4">
+                        
+                        <div className="flex items-center justify-between">
+                            <h2 className="text-black !text-[22px] !font-bold">
+                                Payment Links
+                            </h2>
+
+                            <img src="./src/assets/payment-link-icon.svg" alt="" className="bg-[#2B84EA] rounded-4xl size-12" />
+                        </div>
+
+                        <div className="flex flex-col gap-11">
+                            <p className="!text-[16px] text-[#818597] !leading-6">
+                                Create, Copy & Collect With Payment Button. Collect one time or subscription payments & more
+                            </p>
+
+                            <a href="#" className="text-[16px] font-bold !text-[#0B72E7] hover:text-blue-100 ">
+                                Know More {'>'}
+                            </a>
+                        </div>
+                    </div>
+
+                    {/* Box4 */}
+                    <div className="cursor-pointer !p-5 flex flex-col gap-4">
+                        
+                        <div className="flex items-center justify-between">
+                            <h2 className="text-black !text-[22px] !font-bold">
+                                Payment Links
+                            </h2>
+
+                            <img src="./src/assets/payment-link-icon.svg" alt="" className="bg-[#2B84EA] rounded-4xl size-12" />
+                        </div>
+
+                        <div className="flex flex-col gap-11">
+                            <p className="!text-[16px] text-[#818597] !leading-6">
+                                Create, Copy & Collect With Payment Button. Collect one time or subscription payments & more
+                            </p>
+
+                            <a href="#" className="text-[16px] font-bold !text-[#0B72E7] hover:text-blue-100 ">
+                                Know More {'>'}
+                            </a>
+                        </div>
+                    </div>
+
+                    {/* Box5 */}
+                    <div className="cursor-pointer !p-5 flex flex-col gap-4">
+                        
+                        <div className="flex items-center justify-between">
+                            <h2 className="text-black !text-[22px] !font-bold">
+                                Payment Links
+                            </h2>
+
+                            <img src="./src/assets/payment-link-icon.svg" alt="" className="bg-[#2B84EA] rounded-4xl size-12" />
+                        </div>
+
+                        <div className="flex flex-col gap-11">
+                            <p className="!text-[16px] text-[#818597] !leading-6">
+                                Create, Copy & Collect With Payment Button. Collect one time or subscription payments & more
+                            </p>
+
+                            <a href="#" className="text-[16px] font-bold !text-[#0B72E7] hover:text-blue-100 ">
+                                Know More {'>'}
+                            </a>
+                        </div>
+                    </div>
+
+                    {/* Box6 */}
+                    <div className="cursor-pointer !p-5 flex flex-col gap-4">
+                        
+                        <div className="flex items-center justify-between">
+                            <h2 className="text-black !text-[22px] !font-bold">
+                                Payment Links
+                            </h2>
+
+                            <img src="./src/assets/payment-link-icon.svg" alt="" className="bg-[#2B84EA] rounded-4xl size-12" />
+                        </div>
+
+                        <div className="flex flex-col gap-11">
+                            <p className="!text-[16px] text-[#818597] !leading-6">
+                                Create, Copy & Collect With Payment Button. Collect one time or subscription payments & more
+                            </p>
+
+                            <a href="#" className="text-[16px] font-bold !text-[#0B72E7] hover:text-blue-100 ">
+                                Know More {'>'}
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </section>
     </>
